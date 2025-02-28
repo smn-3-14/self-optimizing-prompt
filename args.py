39a3241467da42dad296245ff872fcd75f6fe004
smn-3-14 <mesimon12@googlemail.com>
2025-02-28 10:27:04 +0100

    add .env.example and clean up

--- args.py
+++ args.py
@@ -9,9 +9,7 @@ def init_arguments():
 
     # Add arguments here
     parser.add_argument("--uni", action='store_true', help="Use uni api")
-    # parser.add_argument("--hf", action='store_true', help="Use hf api")
     parser.add_argument("--code", action='store_true', help="Run code bench")
-    # parser.add_argument("--qa", action='store_true', help="Run qa bench")
     parser.add_argument("--model", type=str, help="which model to use")
     parser.add_argument("--dir", type=str, help="output directory to use")
     parser.add_argument("--baseline", action='store_true', help="Create baseline")
